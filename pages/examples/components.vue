bace172db003b050cd01b06d2aabe154049ba050
樂樂 <andrea@osensetech.com>
2025-10-17 13:45:35 +0800

    新增 component and composable 範例頁面 (#89)
    feat: feat component and composable page

---
+++ pages/examples/components.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="flex w-full flex-col justify-center gap-8 px-6 py-8">
+    <h1 class="text-[36px] font-bold">
+      元件範例
+    </h1>
+    <!-- 從這裡複製模板，註解改放元件名稱 -->
+    <div class="flex w-full flex-col gap-3 rounded-md border border-black p-4">
+      <h2 class="text-[20px] font-bold">
+        這裡放元件名稱
+      </h2>
+      <p>說明用法，若無則不需要</p>
+      <div class="flex gap-4">
+        <!-- 範例元件、各式狀態等 -->
+      </div>
+    </div>
+  </div>
+</template>
